4ef7a28481e3d54171359d273eac949532730330
dev-jb-007 <83109010+dev-jb-007@users.noreply.github.com>
2021-07-21 16:55:57 +0530

    Add files via upload

--- HomePAGE/homepage.js
+++ HomePAGE/homepage.js
@@ -1,73 +1,80 @@
-const li=document.querySelectorAll('#list li');
+const li = document.querySelectorAll('#list li');
 var mediaqueryipad = window.matchMedia("(max-width: 1024px) and (min-width: 500px)")
-let scroll=0;
-function changeactive(element){
-    Array.from(li).forEach(x=>{
+let scroll = 0;
+function changeactive(element) {
+    Array.from(li).forEach(x => {
         x.classList.remove('active');
     });
     element.classList.add('active');
 }
-function expandSearchBar(element){
-    element.style.width='90%';
+function expandSearchBar(element) {
+    element.style.width = '90%';
 }
-function minimizeSearchBar(element){
-    if(window.innerWidth>=500&&window.innerWidth<=1024)
-    {
-        element.style.width='150px';
+function minimizeSearchBar(element) {
+    if (window.innerWidth >= 500 && window.innerWidth <= 1024) {
+        element.style.width = '150px';
     }
-    else{
-        element.style.width='200px';
+    else {
+        element.style.width = '200px';
     }
 }
-function prevOffer(){
-    if(scroll==0)
-    {
-        scroll=-300;
+function prevOffer() {
+    if (scroll == 0) {
+        scroll = -300;
     }
-    else{
-        scroll+=100;
+    else {
+        scroll += 100;
     }
-    document.querySelector('.offer .scroll-image').style.marginLeft=`${scroll}%`;
+    document.querySelector('.offer .scroll-image').style.marginLeft = `${scroll}%`;
 }
-function nextOffer(){
-    if(scroll==-300)
-    {
-        scroll=0;
+function nextOffer() {
+    if (scroll == -300) {
+        scroll = 0;
     }
-    else{
-        scroll-=100;
+    else {
+        scroll -= 100;
     }
-    document.querySelector('.offer .scroll-image').style.marginLeft=`${scroll}%`;
+    document.querySelector('.offer .scroll-image').style.marginLeft = `${scroll}%`;
 }
-function prevView(){
-    if(scroll==0)
-    {
-        scroll=-200;
+function prevView() {
+    if (scroll == 0) {
+        scroll = -200;
     }
-    else{
-        scroll+=100;
+    else {
+        scroll += 100;
     }
-    document.querySelector('.most-viewed .card-scroll').style.marginLeft=`${scroll}%`;
+    document.querySelector('.most-viewed .card-scroll').style.marginLeft = `${scroll}%`;
 }
-function nextView(){
-    if(scroll==-200)
-    {
-        scroll=0;
+function nextView() {
+    if (scroll == -200) {
+        scroll = 0;
     }
-    else{
-        scroll-=100;
+    else {
+        scroll -= 100;
     }
-    document.querySelector('.most-viewed .card-scroll').style.marginLeft=`${scroll}%`;
+    document.querySelector('.most-viewed .card-scroll').style.marginLeft = `${scroll}%`;
 }
-setInterval(()=>{
-    if(scroll==-300)
-    {
-        scroll=0;
+setInterval(() => {
+    if (scroll == -300) {
+        scroll = 0;
     }
-    else{
-        scroll-=100;
+    else {
+        scroll -= 100;
     }
-    document.querySelector('.offer .scroll-image').style.marginLeft=`${scroll}%`;
-},10000);
-
-
+    document.querySelector('.offer .scroll-image').style.marginLeft = `${scroll}%`;
+}, 10000);
+function showmobilenav() {
+    let hamburger = document.querySelector('#mobile-header #nav-hamburger #hamburger');
+    let nav = document.querySelector('.mobile-navbar');
+    if (nav.style.width == '100%') {
+        hamburger.style.backgroundColor = 'black';
+        document.styleSheets[0].addRule('#mobile-header #nav-hamburger #hamburger::after', 'transform:translateY(0px) rotate(0deg)')
+        document.styleSheets[0].addRule('#mobile-header #nav-hamburger #hamburger::before', 'transform:translateY(0px) rotate(0deg)');
+        nav.style.width = '100%';
+        return nav.style.width = '0%'
+    }
+    hamburger.style.backgroundColor = 'transparent';
+    document.styleSheets[0].addRule('#mobile-header #nav-hamburger #hamburger::after', 'transform:translateY(+8px) rotate(45deg)');
+    document.styleSheets[0].addRule('#mobile-header #nav-hamburger #hamburger::before', 'transform:translateY(-8px) rotate(-45deg)');
+    nav.style.width = '100%';
+}
